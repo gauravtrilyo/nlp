310858b11f6fc2f9d689af2dcd5576c63848540b
Gaurav <gaurav@Gauravs-MacBook-Air.local>
2021-02-26 16:02:31 +0530

    Post request

--- server/trainers/nlpjs-trainer.js
+++ server/trainers/nlpjs-trainer.js
@@ -263,24 +263,24 @@ class NlpjsTrainer {
 	 * @param {string} text Utterance text.
 	 */
 	converse(agentId, session, text, serverContext = null) {
-		// console.debug("Server context", serverContext);
+		console.debug("Server context", serverContext);
 
 		const manager = this.managers[agentId];
 		if (!manager) {
 			throw new Error('Unknown manager');
 		}
 		
-		// if (serverContext && Object.keys(serverContext).length > 0) {
-		// 	if (!session.context) {
-		// 		session.context = {}
-		// 	};
-		// 	for (let i in serverContext) {
-		// 		session.context[i] = serverContext[i]
-		// 	}
-		// }
-		// console.log("Session.context", session.context);
+		if (serverContext && Object.keys(serverContext).length > 0) {
+			if (!session.context) {
+				session.context = {}
+			};
+			for (let i in serverContext) {
+				session.context[i] = serverContext[i]
+			}
+		}
+		console.log("Session.context", session.context);
 
-		return manager.process(text, session.context);
+		return manager.process('en', text, session.context);
 	}
 	/**
 	 * Evaluates the string based on condtions
